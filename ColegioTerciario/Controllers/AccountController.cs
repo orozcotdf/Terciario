42d11b7631eac9eeaa938b19bb68b631885eb6fe
Victor Nahuel Chaves <nahuel.chaves@gmail.com>
2015-10-13 21:49:07 -0300

    Permitir login con nombre de usuario

--- ColegioTerciario/Controllers/AccountController.cs
+++ ColegioTerciario/Controllers/AccountController.cs
@@ -85,11 +85,13 @@ namespace ColegioTerciario.Controllers
 
             // No cuenta los errores de inicio de sesión para el bloqueo de la cuenta
             // Para permitir que los errores de contraseña desencadenen el bloqueo de la cuenta, cambie a shouldLockout: true
+            //var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
-                    var user = UserManager.FindByEmail(model.Email);
+                    //var user = UserManager.FindByEmail(model.Email);
+                    var user = UserManager.FindByName(model.Email);
                     var esAlumno = UserManager.IsInRole(user.Id, "Alumno");
                     return esAlumno ? RedirectToAction("Index", "Escritorio", new {area = "Alumnos"}) : RedirectToLocal(returnUrl);
                 case SignInStatus.LockedOut:
